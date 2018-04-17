59d9947bde889282ab426e5c86dafe901d34e883
Holger_Tidemand <tidemand.holger@gmail.com>
2018-04-17 21:04:00 +0200

    adds alot

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-// create a new component. this component should produce some html 
+import YTSearch from 'youtube-api-search';
 
-const App = () => {
-    return <div>Hi!</div>;
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetails from './components/video_details';
+
+
+const API_KEY = 'AIzaSyA_1f2d1fq1V0s2wEVZJCWji6PO5cCcdUk';
+
+class App extends Component  {
+    constructor(props){
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({key: API_KEY, term: 'Surfboards'}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render(){
+        return (
+        <div>
+            <SearchBar />
+            <VideoDetails video={this.state.selectedVideo} />
+            <VideoList
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+                videos={this.state.videos} /> 
+        </div>
+        );
+    }
 }
 
-// take this component's generaded html and put it in the dom.
 
 ReactDOM.render(<App />, document.querySelector('.container'));
